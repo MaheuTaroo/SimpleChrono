9d57db652f70dded77f739fc6dbe9a7a2b81bbea
MaheuTaroo <pantupa@gmail.com>
2023-02-08 00:59:45 +0000

    Attempt to make a chronometer

---
+++ README.md
@@ -0,0 +1,3 @@
+# SimpleChrono
+ 
+This is an attempt to make a simple chronometer for an Android 4.0.4 phone. Right now, the hour/minute/second text views do not change on timer elapse.
---
+++ SimpleChrono/MainActivity.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Timers;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace SimpleChrono
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Timer t = new Timer(1000);
+        int time = 0;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            TableLayout tl = new TableLayout(this);
+
+            /*CustomTextView hrs = new CustomTextView(this, "h"),
+                           mins = new CustomTextView(this, "m"),
+                           secs = new CustomTextView(this, "s");*/
+
+            View[][] views = new View[][] {
+                new View[] {
+                    /*new TextView(this) { Text = "Horas:" },*/
+                    new CustomTextView(this, "h", 1)
+                }, new View[] {
+                    /*new TextView(this) { Text = "Minutos:" },*/
+                    new CustomTextView(this, "m", 2)
+                }, new View[] {
+                    /*new TextView(this) { Text = "Segundos:" },*/
+                    new CustomTextView(this, "s", 3)
+                }, new View[]
+                {
+                    new Button(this) { Text = "Start" },
+                    new Button(this) { Text = "Clear" }
+                }
+            };
+
+            RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MatchParent, RelativeLayout.LayoutParams.WrapContent);
+            lp.AddRule(LayoutRules.Above, 2);
+            views[0][0].LayoutParameters = lp;
+
+            lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MatchParent, RelativeLayout.LayoutParams.WrapContent);
+            lp.AddRule(LayoutRules.Above, 3);
+            lp.AddRule(LayoutRules.Below, 1);
+            views[1][0].LayoutParameters = lp;
+
+            lp = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MatchParent, RelativeLayout.LayoutParams.WrapContent);
+            lp.AddRule(LayoutRules.Below, 2);
+            views[2][0].LayoutParameters = lp;
+
+            views[3][1].Click += (sender, args) =>
+            {
+                for (int i = 0; i < 3; i++)
+                    ((CustomTextView)views[i][0]).Time = 0;
+            };
+            views[3][0].Click += (sender, args) =>
+            {
+                if (((Button)sender).Text == "Start")
+                {
+                    views[3][1].PerformClick();
+                    ((Button)sender).Text = "Stop";
+                    t.Start();
+                }
+                else
+                {
+                    t.Stop();
+                    ((Button)sender).Text = "Start";
+                }
+            };
+            
+
+            foreach (View[] viewArr in views)
+            {
+                TableRow tr = new TableRow(this)
+                {
+                    Orientation = Orientation.Horizontal,
+                    LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+                };
+                
+                foreach (View v in viewArr)
+                {
+                    tr.AddView(v);
+                }
+
+                tl.AddView(tr);
+            }
+
+            t.Elapsed += (sender, args) =>
+            {
+                ((CustomTextView)views[0][0]).Time = time / 3600;
+                ((CustomTextView)views[1][0]).Time = time / 60;
+                ((CustomTextView)views[2][0]).Time = time % 60;
+            };
+
+            SetContentView(tl);
+        }
+    }
+}
+
